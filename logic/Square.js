60276eb706945322f160b56803b9667d9974c86d
Valerii Bihun <valeriibihun.co@gmail.com>
2021-10-30 00:10:20 -0600

    labels are added for 1st row and rank

--- logic/Square.js
+++ logic/Square.js
@@ -3,7 +3,9 @@ import Pieces from  "./pieces.js"
 export default class Square {
     constructor({board, rank, file, isOdd, index}){
         this.board = board;
+        // ranks are rows
         this.rank = rank;
+        // files are columns;
         this.file = file;
         this.index = index;
         this.element = document.createElement(`div`);
@@ -27,12 +29,24 @@ export default class Square {
            if(imageUrl){
                const image = document.createElement('img');
                image.src = imageUrl;
-              
                this.element.append(image);
            }else{
                this.element.textContent = current.type;
            }
        }
-       
+    //    adding labels for bottom rank A - H
+       if (this.rank === 1){
+           const label = document.createElement('span');
+           label.classList.add('label');
+           label.textContent = this.file;
+           this.element.append(label);
+       }
+    //    adding labels for right file 1 - 8
+       if (this.file === 'H'){
+           const fileLabel = document.createElement('span');
+           fileLabel.classList.add('fileLabel');
+           fileLabel.textContent = this.rank;
+           this.element.append(fileLabel);
+       }
     }
 }
--- logic/pieces.js
+++ logic/pieces.js
@@ -3,7 +3,7 @@ export default {
     bk: 'https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg',
     bq: 'https://upload.wikimedia.org/wikipedia/commons/4/47/Chess_qdt45.svg',
     br: 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Chess_rdt45.svg',
-    bb: 'https://upload.wikimedia.org/wikipedia/commons/9/98/Chess_bdt45.svg',
+    bb: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Chess_edt45.svg/1024px-Chess_edt45.svg.png',
     bn: 'https://upload.wikimedia.org/wikipedia/commons/e/ef/Chess_ndt45.svg',
     bp: 'https://upload.wikimedia.org/wikipedia/commons/c/c7/Chess_pdt45.svg',
 
@@ -11,7 +11,7 @@ export default {
     wk: 'https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg',
     wq: 'https://upload.wikimedia.org/wikipedia/commons/1/15/Chess_qlt45.svg',
     wr: 'https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg',
-    wb: 'https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg',
+    wb: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Chess_elt45.svg/1024px-Chess_elt45.svg.png',
     wn: 'https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg',
     wp: 'https://upload.wikimedia.org/wikipedia/commons/4/45/Chess_plt45.svg',
   };
